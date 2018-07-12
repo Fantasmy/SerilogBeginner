9651ac456dc1a5c35f775ce32f1f86b4ef37795d
Jenny <fantazmit@gmail.com>
2018-07-12 13:24:04 +0200

    start

---
+++ SerilogBeginner/Program.cs
@@ -0,0 +1,32 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerilogBeginner
+{
+    class Program
+    {
+            public static void Main(string[] args)
+            {
+            //using (var log = new LoggerConfiguration()
+            //    .WriteTo.Console()
+            //    .CreateLogger())
+            //{
+            //    log.Information("Hello, Serilog!");
+            //    log.Warning("Goodbye, Serilog.");
+            //}
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Information("Hello again, Serilog!");
+
+            Log.CloseAndFlush();
+        }
+        
+    }
+}
